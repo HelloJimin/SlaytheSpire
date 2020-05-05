3a01cb876ba290f7c392a5cc21880dcc9d929ec6
HelloJimin <60608212+HelloJimin@users.noreply.github.com>
2020-05-05 23:12:51 +0900

    첫번째 확인용

---
+++ Assets/Script/CardDarg.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class CardDarg : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    public static Vector2 originalPosition;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        originalPosition = transform.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 currentPos = Input.mousePosition;
+        transform.position = currentPos;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.position = originalPosition;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/DeckList.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckList : MonoBehaviour
+{
+    public static DeckList instance;
+
+    public GameObject card;
+    public Queue<Card> deckList = new Queue<Card>();
+
+    void Awake()
+    {
+        Cardinit();
+    }
+
+    void Cardinit()
+    {
+        instance = this;
+
+        for (int i = 0; i < 20; i++)
+        {
+            deckList.Enqueue(CreateCard());
+        }
+        Debug.Log("초기 카드 생성 완료");
+    }
+
+    Card CreateCard()
+    {
+        Card newCard = Instantiate(card).GetComponent<Card>();
+        newCard.transform.position = transform.position;
+        newCard.transform.SetParent(transform);
+        newCard.gameObject.SetActive(true);
+        return newCard;
+    }
+
+    public static Card getCard()
+    {
+        if (instance.deckList.Count>0)
+        {
+            var card = instance.deckList.Dequeue();
+            card.transform.SetParent(null);
+            return card;
+        }
+        else
+        {
+            var card = instance.CreateCard();
+            card.transform.SetParent(null);
+            return card;
+        }
+    }
+}
