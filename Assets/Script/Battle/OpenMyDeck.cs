59c0eb9bb9c9bdb50b46f2f57394a0333389be0a
HelloJimin <60608212+HelloJimin@users.noreply.github.com>
2020-05-07 22:37:55 +0900

    별거안함
    hp바 수정해야되고 렌더순서 어케하지

---
+++ Assets/Script/Battle/OpenMyDeck.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenMyDeck : MonoBehaviour
+{
+    bool isOpen;
+    RectTransform[] childs;
+    private void Awake()
+    {
+        isOpen = false;
+        childs = GetComponentsInChildren<RectTransform>();
+    }
+
+    public void click()
+    {
+        if (isOpen)
+        {
+            isOpen = false;
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            isOpen = true;
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.SetActive(true);
+            }
+        }
+    }
+}
